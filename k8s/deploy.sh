39c62d009bc10555f29a8d6635f368251f3736a2
Ciber1a-Moha <grupo7a@controlplane>
2026-02-04 09:49:28 +0000

    Fixed issues con k8s

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #1 secretos
-kubectl apply -f /home/vagrant/SneakerShop/k8s/secrets.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/secrets.yml
 
 #2 volumenes
-kubectl apply -f /home/vagrant/SneakerShop/k8s/pv_volume.yml
-kubectl apply -f /home/vagrant/SneakerShop/k8s/pvc_volume.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/pv_volume.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/pvc_volume.yml
 
 #3 mariadb
-kubectl apply -f /home/vagrant/SneakerShop/k8s/mariadb_configmap.yml
-kubectl apply -f /home/vagrant/SneakerShop/k8s/mariadb_deployment.yml
-kubectl apply -f /home/vagrant/SneakerShop/k8s/mariadb_service.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/mariadb_configmap.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/mariadb_deployment.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/mariadb_service.yml
 
 #4 python
-kubectl apply -f /home/vagrant/SneakerShop/k8s/python_deployment.yml
-kubectl apply -f /home/vagrant/SneakerShop/k8s/python_service.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/python_deployment.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/python_service.yml
 
 #5 apache
-kubectl apply -f /home/vagrant/SneakerShop/k8s/apache_deployment.yml
-kubectl apply -f /home/vagrant/SneakerShop/k8s/apache_service.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/apache_deployment.yml
+kubectl apply -f /home/grupo7a/SneakerShop/k8s/apache_service.yml
 
 #6 phpmyadmin
-kubectl apply -f /home/vagrant/SneakerShop/k8s/phpmyadmin_deployment.yml
-kubectl apply -f /home/vagrant/SneakerShop/k8s/phpmyadmin_service.yml
+#kubectl apply -f /home/grupo7a/SneakerShop/k8s/phpmyadmin_deployment.yml
+#kubectl apply -f /home/grupo7a/SneakerShop/k8s/phpmyadmin_service.yml
