17789b6e3b3f80b80af211f770cc6cf8f19d805a
zoltar238 <protoandrei@protonmail.com>
2026-05-12 10:07:06 +0200

    agregadas validaciones xss

--- api/web/rutas_comentarios.py
+++ api/web/rutas_comentarios.py
@@ -12,7 +12,7 @@ bp = Blueprint('comentarios', __name__)
 def login():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        comentario_json = request.json
+        comentario_json = request.cleaned.json
         usuario = comentario_json['usuario']
         descripcion = comentario_json['descripcion']
         respuesta,code= controlador_comentarios.insertar_comentario(usuario,descripcion)
--- api/web/rutas_usuarios.py
+++ api/web/rutas_usuarios.py
@@ -14,7 +14,7 @@ bp = Blueprint('usuarios', __name__)
 def login():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        login_json = request.json
+        login_json = request.cleaned.json
         username = login_json['username']
         password = login_json['password']
         respuesta,code= controlador_usuarios.login_usuario(username,password)
@@ -28,7 +28,7 @@ def login():
 def registro():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        login_json = request.json
+        login_json = request.cleaned.json
         username = login_json['username']
         password = login_json['password']
         profile = login_json['profile']
